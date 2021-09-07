ae04cc742cc034084f8bb7547c14276da9a705cf
unknown <catrosits@gmail.comgit config --global user.name>
2021-09-07 13:10:19 +0800

    validation with jwt and httpOnly cookies

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res) => {
 	const {
@@ -34,7 +35,7 @@ export const signUp = async (req, res) => {
 		// hash the password using bcrypt
 		const salt = bcrypt.genSaltSync(10);
 		const passwordHash = await bcrypt.hash(password, salt);
-
+		// create new user model
 		const newUser = new User({
 			firstName,
 			lastName,
@@ -43,9 +44,25 @@ export const signUp = async (req, res) => {
 			passwordHash
 		});
 
-		const createUser = await newUser.save();
+		const createdUser = await newUser.save();
 
-		res.status(200).send(createUser);
+		// log user automatically after signup
+		const token = jwt.sign(
+			{
+				user: createdUser._id
+			},
+			process.env.JWT_SECRET,
+			{ expiresIn: "1h" }
+		);
+
+		//send token to http only cookie
+		res
+			.cookie("mernauth", token, {
+				httpOnly: true
+			})
+			.send();
+
+		res.status(200).send(createdUser);
 	} catch (error) {
 		console.log(error.message);
 	}
